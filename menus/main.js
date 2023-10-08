64fca02729ab875881775025fc2429cf3a4419a6
zhoudkailie001 <146654704+zhoudkailie001@users.noreply.github.com>
2023-10-08 09:15:48 +0000

    Add files via upload

---
+++ menus/main.js
@@ -0,0 +1,28 @@
+/* 
+function simpleInterest(principal, time, rate){
+  let simpleInterest = principal * time * rate /100;
+  console.log(`The S I is: ${simpleInterest}`);
+}
+
+console.log('This is my field coder and technicial');
+
+simpleInterest(2, 10, 14); 
+*/
+
+
+
+
+
+
+/* 
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+const tagName = document.getElementsByTagName('li');
+console.log(tagName);
+
+const whole = document.querySelectorAll('div');
+console.log(whole); 
+*/
+
+
